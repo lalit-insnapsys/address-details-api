e163b1a0f3ed24f605a9488906c9762a4790346d
lalit-insnapsys <lalit.t.insnapsys@gmail.com>
2025-02-25 15:25:14 +0530

    Changed base urls and added API to fetch streets for district

--- addressDetailsApi/paris/urls.py
+++ addressDetailsApi/paris/urls.py
@@ -3,5 +3,6 @@ from . import views # Importing the views module from the current package
 
 
 urlpatterns = [
-    path('', views.get_districts_list, name='paris-data'), # URL pattern for the Districts view
+    path('', views.get_districts_list, name='districts-data'), # URL pattern for the Districts view
+    path('<int:district_code>/', views.get_streets_by_district_code, name='streets-data'), # URL pattern for the Streets view
 ]
--- addressDetailsApi/paris/views.py
+++ addressDetailsApi/paris/views.py
@@ -1,14 +1,19 @@
 # standard library imports
 import os
 import requests
+import re
+from urllib.parse import quote_plus
+
+from django.http import JsonResponse  # Importing JsonResponse to send JSON responses
 
 # django imports
 from rest_framework.decorators import api_view  # Importing api_view decorator from Django REST framework
 from rest_framework.response import Response  # Importing Response to send HTTP responses
-from rest_framework import status  # Importing status to use HTTP status codes
+from rest_framework.request import Request  # Importing Request to send HTTP responses
+from rest_framework import status
 
 @api_view(['GET'])
-def get_districts_list(request: 'Request') -> Response:
+def get_districts_list(request: Request) -> Response:
     """
     Handles GET requests to fetch the list of districts from an external API.
     
@@ -27,4 +32,28 @@ def get_districts_list(request: 'Request') -> Response:
 
     except requests.exceptions.RequestException as e:
         # Handling any request exceptions and returning a 500 Internal Server Error status
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+@api_view(['GET'])
+def get_streets_by_district_code(request: Request, district_code: int) -> Response:
+    """
+    Fetches the list of streets based on district code.
+
+    Args:
+        request (Request): The HTTP request object.
+        district_code (int): The district code to filter streets.
+
+    Returns:
+        Response: A Response object containing the data or an error message.
+    """
+    BASE_URL: str = os.getenv("STREETS_DATA_URL")  # Getting the base URL from environment variables
+    formatted_district = f"{district_code:02d}e"  # Ensure it's zero-padded (e.g., 1 -> 01e, 2 -> 02e)
+    query_params = {"refine.arrdt": formatted_district}  # Formatting district code properly
+
+    try:
+        response = requests.get(BASE_URL, params=query_params)
+        response.raise_for_status()  # Raises an error for bad responses (4xx, 5xx)
+        return Response(response.json(), status=status.HTTP_200_OK)
+
+    except requests.exceptions.RequestException as e:
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
